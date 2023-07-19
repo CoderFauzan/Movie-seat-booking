25f59ff6cef9c176bf464953210147f777aa4e39
CoderFauzan <90621630+CoderFauzan@users.noreply.github.com>
2023-07-19 11:29:20 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.scss';
 
-import { useState } from "react";
-
+import { useState } from 'react';
 
 export default function App() {
-  const [seats, setSeats] = useState(Array(50).fill("seat"));
+  const [seats, setSeats] = useState(Array(50).fill('seat'));
 
   const handleSeatClick = (index) => {
     const newSeats = [...seats];
-    if (newSeats[index] === "seat") {
-      newSeats[index] = "reserve";
-    } else if (newSeats[index] === "reserve") {
-      newSeats[index] = "seat";
+    if (newSeats[index] === 'seat') {
+      newSeats[index] = 'reserve';
+    } else if (newSeats[index] === 'reserve') {
+      newSeats[index] = 'seat';
     }
     setSeats(newSeats);
-};
-
+  };
 
   const handleResetClick = () => {
-    setSeats(Array(50).fill("seat"));
+    setSeats(Array(50).fill('seat'));
   };
 
   const handleClearClick = () => {
-    setSeats(Array(50).fill("seat"));
+    setSeats(Array(50).fill('seat'));
   };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const reservedSeats = seats.filter((seat) => seat === "reserve");
+    const reservedSeats = seats.filter((seat) => seat === 'reserve');
     if (reservedSeats.length === 0) {
-      alert("No seats booked!");
+      alert('No seats booked!');
     } else {
       alert(`Reserved seats: ${reservedSeats}`);
     }
-};
+  };
 
   return (
     <div className="App">
@@ -43,7 +41,7 @@ export default function App() {
         {seats.map((seat, index) => (
           <button
             key={index}
-            className={`${seat} ${seat === "seat" ? "green" : ""}`}
+            className={`${seat} ${seat === 'seat' ? 'green' : ''}`}
             onClick={() => handleSeatClick(index)}
           >
             {index + 1}
@@ -58,5 +56,5 @@ export default function App() {
         <button onClick={handleClearClick}>Clear</button>
       </div>
     </div>
-);
-        }
+  );
+}
